fe0279544d6f6e1bd7d42fedfd338c8851fdc708
FNandaSilvaMendonca123457 <104873663+FNandaSilvaMendonca123457@users.noreply.github.com>
2023-03-09 21:47:26 -0300

    Update environment.ts

--- appteste/src/environments/environment.ts
+++ appteste/src/environments/environment.ts
@@ -1,12 +1,14 @@
+
 export const environment = {
   production: false,
-  apiUrl: 'https://thronesapi.com/api/v2/',
-  authorizationUrl: 'https://18.213.134.105:9443/api/identity/entitlement/decision',
-
-  appName: 'App ionic - Game of Thrones',
+  apiUrl: 'https://thronesapi.com/api/v2',
+  authorizationUrl: 'https://ssodesenv.go.gov.br:443/api/identity/entitlement/decision',
+  apiPortalRS: '/acesso-usuario-api',
+  idSistemaPortal: 'SMART-NG',
+  appName: 'app iIonic - Game of Thrones',
   sso: {
-    clientId: 'cODNHELi2yZRBgJBS1RybZ9yS1Aa',
-    serverUrl: 'https://18.213.134.105:9443',
+    clientId: 'qruk19U0vzZZ27mz4DhD1i49CNUa',
+    serverUrl: 'https://3.222.218.189:9443',
     issuer: '/oauth2/oidcdiscovery',
     redirectUri: window.location.origin,
     scope: 'openid email profile',
